000ad60b288b26a0f54688edf34d4949c3416221
Kenten Roth <kentencroth@yahoo.com>
2025-05-30 15:26:24 -0500

    Added requirements

--- CookBook/Models/AppUser.cs
+++ CookBook/Models/AppUser.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace CookBook.Models;
 
 public class AppUser : IdentityUser
 {
+    [Required]
+    [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
 }
--- CookBook/Program.cs
+++ CookBook/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddControllers();
 
 builder.Services.AddValidatorsFromAssemblyContaining<AppUserValidator>();
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
