acfbdf24b68e3e12147a5f464228ee5c7c725525
Kenten Roth <kentencroth@yahoo.com>
2025-06-08 07:05:23 -0500

    Updating refresh token model

--- CookBook/Models/RefreshTokens.cs
+++ CookBook/Models/RefreshTokens.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CookBook.Models;
 
 public class RefreshTokens
 {
     public int Id { get; set; }
+    
+    [MaxLength(512)]
     public string Token { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime ExpiresAt { get; set; }
+    
+    [MaxLength(45)]
+    public string CreatedByIp { get; set; } = string.Empty;
+    
+    [MaxLength(45)]
+    public string? RevokedByIp { get; set; }
+    public DateTime? RevokedAt { get; set; }
     public bool IsRevoked { get; set; }
     
     public string UserId { get; set; } = string.Empty;
