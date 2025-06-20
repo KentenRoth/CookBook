7dd360331b7ea8ca8556dfed1130c4c5cb864635
Kenten Roth <kentencroth@yahoo.com>
2025-06-19 19:39:44 -0500

    Validate Refresh Token

--- CookBook/Services/TokenService.cs
+++ CookBook/Services/TokenService.cs
@@ -102,11 +102,9 @@ public class TokenService : ITokenService
         var tokenEntity = await _context.RefreshTokens
             .FirstOrDefaultAsync(t => t.Token == refreshToken);
 
-        if (tokenEntity != null)
+        if (tokenEntity == null || tokenEntity.IsRevoked || tokenEntity.ExpiresAt < DateTime.UtcNow)
         {
-            tokenEntity.IsRevoked = true;
-            _context.RefreshTokens.Update(tokenEntity);
-            await _context.SaveChangesAsync();
+            throw new SecurityTokenException("Invalid or expired refresh token");
         }
     }
 }
