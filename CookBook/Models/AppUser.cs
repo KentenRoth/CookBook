f02182a14e275eedf9960fd9cf3aee7ba15292e7
Kenten Roth <kentencroth@yahoo.com>
2025-07-04 07:05:43 -0500

    Added created at to User model

--- CookBook/Models/AppUser.cs
+++ CookBook/Models/AppUser.cs
@@ -8,6 +8,7 @@ public class AppUser : IdentityUser
     [Required]
     [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
     public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
     public ICollection<RefreshTokens> RefreshTokens { get; set; } = new List<RefreshTokens>();
