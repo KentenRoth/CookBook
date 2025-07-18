e6fa808607924bf07beeb09d7ac7309d7f3b7279
Kenten Roth <kentencroth@yahoo.com>
2025-07-18 11:43:33 -0500

    Cleanup

--- CookBook/DTOs/Account/Response/MeAccountResponseDto.cs
+++ CookBook/DTOs/Account/Response/MeAccountResponseDto.cs
@@ -3,9 +3,9 @@ namespace CookBook.DTOs.Account.Response;
 public class MeAccountResponseDto
 {
     public string Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
     public DateTime CreatedAt { get; set; }
     public object UserSettings { get; set; } = new object();
     
