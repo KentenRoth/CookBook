35f775cd3e396843fa7d1050e5316440c137dec6
Kenten Roth <kentencroth@yahoo.com>
2025-10-27 17:59:59 -0500

    Fixed typo

---
+++ CookBook/Migrations/20251027221844_typoFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class typoFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PrrofileImageUrl",
+                table: "UserSettings",
+                newName: "ProfileImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfileImageUrl",
+                table: "UserSettings",
+                newName: "PrrofileImageUrl");
+        }
+    }
+}
