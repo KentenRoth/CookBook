47d9f553d8a42722182df35a816f31766227e3ec
Kenten Roth <kentencroth@yahoo.com>
2025-11-15 11:43:23 -0600

    Recipe Image migration

---
+++ CookBook/Migrations/20251115174208_recipe-image.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class recipeimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RecipeImageUrl",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecipeImageUrl",
+                table: "Recipes");
+        }
+    }
+}
